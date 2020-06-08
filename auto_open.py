8068233871a0838f0328e558578caf20c66cb8af
xuyou1999 <yx1711@nyu.edu>
2020-06-08 20:19:31 +0800

    enter

--- auto_open.py
+++ auto_open.py
@@ -1,12 +1,22 @@
 import webbrowser
-def auto_open(filename):
+def auto_open(filename, start, end):
     f = open(filename, 'r')
     urls = f.readlines()
-    for url in urls:
+    for i in range(start - 1, end):
+        url = urls[i]
         url = url.strip()
         webbrowser.open(url)
     f.close()
 
+def get_total_line(filename):
+    f = open(filename, 'r')
+    urls = f.readlines()
+    return len(urls)
+
 if __name__ == "__main__":
     filename = input('filename? ')
-    auto_open(filename)
+    total_line = get_total_line(filename)
+    print('There are in total', total_line, 'lines')
+    startline = int(input('start line? '))
+    endline = int(input('end line? '))
+    auto_open(filename, startline, endline)
